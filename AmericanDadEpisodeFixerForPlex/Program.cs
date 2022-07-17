e8b976dd28a2a00d4401921786e31815fcb2d6dd
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-07-17 12:07:09 -0400

    will pull from the wiki

--- AmericanDadEpisodeFixerForPlex/Program.cs
+++ AmericanDadEpisodeFixerForPlex/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+const string EPISODE_LIST_DATA = "https://en.wikipedia.org/wiki/List_of_American_Dad!_episodes";
+
+
+HttpClient client = new HttpClient();
+
+//would like to pull from plex since it's better but i'm not sure how there api works
+//and this is supposto be quick so i'm gonna pull from wikipea and handle it my own way
+var response = await client.GetAsync(EPISODE_LIST_DATA);
+
+if(response.IsSuccessStatusCode)
+{
+
+}
+else
+{
+    Console.WriteLine("Could not pull episode list data")
+}
